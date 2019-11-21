32cf5acb1e51cf834a411e898994067c6878f029
FilipSwiatek <fililswiatek.fil@gmail.com>
2019-11-21 14:10:01 +0100

    dodanie error GPIO

--- targets/arm/Inc/main.h
+++ targets/arm/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define ERROR_GPIO_Pin GPIO_PIN_13
+#define ERROR_GPIO_GPIO_Port GPIOC
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
