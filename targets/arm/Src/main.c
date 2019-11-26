3f18389f9212d5f0da6f9eb3b8efd6b810f8d4ce
FilipSwiatek <filipswiatek.fil@gmail.com>
2019-11-26 18:55:20 +0100

    dodanie sterowania wsaźnikami i driverem multipleksera

--- targets/arm/Src/main.c
+++ targets/arm/Src/main.c
@@ -6,8 +6,12 @@
 #include "clock.h"
 #include <stdbool.h>
 
-
+// handler błędu (nie mylić z hardfaultem)
 void Error_Handler(void);
+// cosekundowe mruganie diodą
+static void ErrorDiodeBlinkingProc();
+static bool switchTarget(uint8_t targetNo);
+void targetTest(void);
 
 
 int main(void) {
@@ -16,34 +20,104 @@ int main(void) {
     if(USART1_UART_Init() == false) Error_Handler();
     if(USB_DEVICE_Init() == false) Error_Handler();
 
-    uint32_t previousTick = HAL_GetTick();
-
-    while(HAL_GetTick() > previousTick + 1000);
-
-    previousTick = HAL_GetTick();
-    char c;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
     while (1) {
-
-        if(USART_GetChar(&c)){
-            USART_PutChar(c);
-        }
         USB_Proc(); // potrzebne do optymalizacji wysyłu danych przez USB (zbieranie danych w większe pakiety - coś na wzór algorytmu Nagle'a)
-
-        if(HAL_GetTick() > previousTick + 1000){
-            previousTick = HAL_GetTick();
-            USB_VCOM_PutChar('c');
-        }
-
-if(USB_VCOM_GetChar(&c)){
-    USB_VCOM_PutChar(c);
-    HAL_GPIO_TogglePin(ERROR_GPIO_GPIO_Port, ERROR_GPIO_Pin);
-}
+        ErrorDiodeBlinkingProc(); // kontrola działąnia backgroundu
+        targetTest();
     }
+#pragma clang diagnostic pop
+
 }
 
 
 void Error_Handler(void){
     HAL_GPIO_WritePin(ERROR_GPIO_GPIO_Port, ERROR_GPIO_Pin, GPIO_PIN_RESET);
+}
+
+static void ErrorDiodeBlinkingProc(){
+    static uint32_t previousTick;
+    if(HAL_GetTick() - previousTick > 1000){
+        previousTick = HAL_GetTick();
+        HAL_GPIO_TogglePin(ERROR_GPIO_GPIO_Port, ERROR_GPIO_Pin);
+    }
+}
+
+static bool switchTarget(uint8_t targetNo){
+    if(targetNo > 7){
+        return false;
+    }
+
+    if(targetNo & (1U<<0U)){
+        HAL_GPIO_WritePin(TARGET_SELECT_0_MUX_DRIVER_PORT, TARGET_SELECT_0_MUX_DRIVER_PIN, GPIO_PIN_SET);
+    }else{
+        HAL_GPIO_WritePin(TARGET_SELECT_0_MUX_DRIVER_PORT, TARGET_SELECT_0_MUX_DRIVER_PIN, GPIO_PIN_RESET);
+    }
+
+    if(targetNo & (1U<<1U)){
+        HAL_GPIO_WritePin(TARGET_SELECT_1_MUX_DRIVER_PORT, TARGET_SELECT_1_MUX_DRIVER_PIN, GPIO_PIN_SET);
+    }else{
+        HAL_GPIO_WritePin(TARGET_SELECT_1_MUX_DRIVER_PORT, TARGET_SELECT_1_MUX_DRIVER_PIN, GPIO_PIN_RESET);
+    }
+
+    if(targetNo & (1U<<2U)){
+        HAL_GPIO_WritePin(TARGET_SELECT_2_MUX_DRIVER_PORT, TARGET_SELECT_2_MUX_DRIVER_PIN, GPIO_PIN_SET);
+    }else{
+        HAL_GPIO_WritePin(TARGET_SELECT_2_MUX_DRIVER_PORT, TARGET_SELECT_2_MUX_DRIVER_PIN, GPIO_PIN_RESET);
+    }
+
+    // turning off channel indicators
+    HAL_GPIO_WritePin(CH0_INDICATOR_PORT, CH0_INDICATOR_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CH1_INDICATOR_PORT, CH1_INDICATOR_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CH2_INDICATOR_PORT, CH2_INDICATOR_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CH3_INDICATOR_PORT, CH3_INDICATOR_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CH4_INDICATOR_PORT, CH4_INDICATOR_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CH5_INDICATOR_PORT, CH5_INDICATOR_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CH6_INDICATOR_PORT, CH6_INDICATOR_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CH7_INDICATOR_PORT, CH7_INDICATOR_PIN, GPIO_PIN_RESET);
+
+    switch(targetNo){
+        case 0:
+            HAL_GPIO_WritePin(CH0_INDICATOR_PORT, CH0_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+        case 1:
+            HAL_GPIO_WritePin(CH1_INDICATOR_PORT, CH1_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+        case 2:
+            HAL_GPIO_WritePin(CH2_INDICATOR_PORT, CH2_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+        case 3:
+            HAL_GPIO_WritePin(CH3_INDICATOR_PORT, CH3_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+        case 4:
+            HAL_GPIO_WritePin(CH4_INDICATOR_PORT, CH4_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+        case 5:
+            HAL_GPIO_WritePin(CH5_INDICATOR_PORT, CH5_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+        case 6:
+            HAL_GPIO_WritePin(CH6_INDICATOR_PORT, CH6_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+        case 7:
+            HAL_GPIO_WritePin(CH7_INDICATOR_PORT, CH7_INDICATOR_PIN, GPIO_PIN_SET);
+            break;
+    }
+
+       return true;
+}
+
+void targetTest(void){
+    static uint32_t previousTick;
+    static uint8_t target;
+
+    if(HAL_GetTick() - previousTick > 1000){
+        previousTick = HAL_GetTick();
+        //mięsko
+        switchTarget(target);
+        target++;
+        target = target % 8;
+    }
 }
 
 
