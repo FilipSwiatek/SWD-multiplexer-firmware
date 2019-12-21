8aa66f6dd3ac0ca0fad4aeb75629ffbd4005e626
FilipSwiatek <fililswiatek.fil@gmail.com>
2019-12-21 13:49:20 +0100

    finalna wersja testu

--- SWD_MUX.py
+++ SWD_MUX.py
@@ -3,19 +3,40 @@ import serial
 import time
 
 
-def jlink_download(targetname='STM32F103C8', hexname='arm.hex'):
-    jlink_script = open('script.jlink', 'w')  # open J-Link script file
-    jlink_script.write('h\nerase\nloadfile ' + hexname + '\nr\nq')  # fulfil J-Link script file
-    jlink_script.close()  # close J-Link script file
-    os.system('jlink -device ' + targetname + ' -if SWD -speed 4000 -autoconnect 1 -CommanderScript script.jlink')  # execute J-Link erase&flash command based on J-Link script file
-    os.remove("script.jlink")  # remove J-Link script file
+def jlink_download(targetname, hexname, erase=True, speed=4000):
 
-debug_print = 1
+    jlink_script = open('script.jlink', 'w')  # open J-Link script file
+    jlink_script.write('h\n')
+    if erase:
+        jlink_script.write('erase\n')
+    jlink_script.write('loadfile ' + hexname + '\nr\nq')  # fulfil J-Link script file
+    # close J-Link script file
+    jlink_script.close()
+    # execute J-Link erase&flash command based on J-Link script file
+    os.system('jlink -device '
+              + targetname +
+              ' -if SWD -speed ' + str(speed) +
+              ' -autoconnect 1 -CommanderScript script.jlink >> jlink.log')
+    # remove J-Link script file
+    os.remove("script.jlink")
+    #searching for special strings in J-link log to recognize failure
+    ret = 0
+    jlink_log = open('jlink.log', 'r')
+    if 'Cannot connect to target.' in jlink_log.read():
+        ret = 1
+
+    jlink_log.close()
+    #os.remove('jlink.log')
+    return ret
+
+
 
 class Multiplexer:
     def __init__(self, com_port='COM1'):
         self.serial = serial.Serial(com_port, 115200, timeout=0.1)  # open serial port
         self._target_no = 0
+        self.serial.write(b'\n')
+        self.serial.read(2137)
 
     def __del__(self):
         self.serial.close()
@@ -24,20 +45,24 @@ class Multiplexer:
         if target_no < 0 or target_no > 7:
             return 0
         self._target_no = target_no
-        self.serial.write(b'select ' + str(self._target_no).encode('utf-8') + b'\n')
-        print('selecting target #' + str(self._target_no))
-        feedback = self.serial.readline().decode()
-        if feedback != 'OK':  # resend if error
+        feedback = 'ERR'
+        sending_counter = 0
+        while 'OK' not in feedback:
             self.serial.write(b'select ' + str(self._target_no).encode('utf-8') + b'\n')
-        print(self.serial.readline().decode())
+            print('selecting target #' + str(self._target_no))
+            feedback = self.serial.readline().decode()
+            sending_counter += 1
+            assert (sending_counter < 4), 'more than 2 resends of command'
 
     def reset_all(self, state=''):
-        self.serial.write(b'reset_all ' + state.encode('utf-8') + b'\n')
-        print('reseting all targets with option:', state)
-        feedback = self.serial.readline().decode()
-        if feedback != 'OK':  # resend if error
+        feedback = 'ERR'
+        sending_counter = 0
+        while 'OK' not in feedback:
             self.serial.write(b'reset_all ' + state.encode('utf-8') + b'\n')
-        print(self.serial.readline().decode())
+            print('reseting all targets with option:', state)
+            feedback = self.serial.readline().decode()
+            sending_counter += 1
+            assert (sending_counter < 4), 'more than 2 resends of command'
 
 
 def select_test(mux):
@@ -50,34 +75,30 @@ def select_test(mux):
         time.sleep(0.1)
 
 
-def download_test(mux, target_no=0, target_name='STM32F103C8', hex_name='arm.hex'):
+def download_test(mux, target_name, hex_name, target_no, erase, speed):
     mux.select_target(target_no)
-    jlink_download(target_name, hex_name)
+    jlink_download(target_name, hex_name, erase, speed)
 
 
 def reset_test(mux):
     mux.reset_all('set')
-    time.sleep(0.5)
+    time.sleep(0.1)
     mux.reset_all('clear')
-    time.sleep(0.5)
     mux.reset_all()
 
 
-def test(mux_handle, binary, target_device='?'):
-    select_test(mux_handle)
-    reset_test(mux_handle)
-
-    download_test(mux_handle, 0, target_device, binary)
-    download_test(mux_handle, 1, target_device, binary)
+def select_and_download(mux, target_no, binary, target_device='?', erase=True, speed=4000):
+    mux.select_target(target_no)
+    return jlink_download(target_device, binary, erase, speed)
 
 
-target = 'EZR32WG330F256RXX'
-target_binary = 'target.hex'
-connected_mux = Multiplexer('COM18')
-
-test(connected_mux, target_binary, target)
-
-connected_mux.__del__()
-
-
-
+def test(mux, binary, target_device='?'):
+    select_test(mux)
+    reset_test(mux)
+    tests_per_port = 10
+    used_ports = 1
+    for current_port in range(used_ports):
+        errors_per_port = 0
+        for x in range(tests_per_port):
+            errors_per_port += select_and_download(mux, current_port, binary, target_device, True, 9000)
+        print('errors per port #' + str(current_port) + ' =', errors_per_port)
